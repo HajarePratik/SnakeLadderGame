3e37dfa31444e0f9dc708b4af82f95111236d722
HajarePratik <pratikhajare09@gmail.com>
2021-05-27 14:38:22 +0530

    Rolling a Dies

--- SnakeLadderGame.java
+++ SnakeLadderGame.java
@@ -5,5 +5,8 @@ public class SnakeLadderGame
 		System.out.println("Welcome to the Snake and Ladder Game");
 		
 		int startposition=0;
+		
+		int DieRoll = (int) (Math.random()*6)+1;
+		System.out.println(DieRoll);
 	}
 }
