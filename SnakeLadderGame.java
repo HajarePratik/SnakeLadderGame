8ca5bf06c2ee1b91a525fefcf15bc7a4e26f1037
HajarePratik <pratikhajare09@gmail.com>
2021-05-27 14:50:30 +0530

    Getting Postion of Player

--- SnakeLadderGame.java
+++ SnakeLadderGame.java
@@ -1,12 +1,38 @@
 public class SnakeLadderGame 
 {
+	public static final int NO_PLAY = 0 ;
+	public static final int LADDER = 1 ;
+	public static final int SNAKE = 2 ;
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to the Snake and Ladder Game");
 		
 		int startposition=0;
+		int newposition=0;
 		
 		int DieRoll = (int) (Math.random()*6)+1;
-		System.out.println(DieRoll);
-	}
+		int CheckOption = (int) (Math.random()*3);
+		
+			switch (CheckOption) 
+			{
+				case NO_PLAY : 
+					newposition = 0;
+					break;
+				case LADDER :
+					newposition = DieRoll ;
+					break;
+				case SNAKE :
+					newposition = -DieRoll ;
+					break;
+			}
+			
+			startposition = startposition + newposition;
+		
+			if (startposition<0)
+			{
+				startposition = 0;
+			}
+			System.out.println("Your are Now at "+startposition+"th"+" Position");
+	} 
 }
